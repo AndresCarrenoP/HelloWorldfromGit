bfd9e2acad1a05f748e60f642bda2c7d0c3bcc34
AndresCarrenoP <andres.carreno@live.com>
2021-03-03 21:04:19 -0600

    haciendo el primer commit/push para subir a github

---
+++ src/MainmyHelloWorld.java
@@ -0,0 +1,8 @@
+public class MainmyHelloWorld {
+    public static void main (String []args){
+        System.out.println("Print this Hello World!!!");
+
+        // use git diff --staged to see changes in staged
+    }
+
+}
