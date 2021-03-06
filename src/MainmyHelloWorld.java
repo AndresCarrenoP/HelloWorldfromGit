e5bb449468e2ba72520dc92fd8a912b4cee772c5
AndresCarrenoP <andres.carreno@live.com>
2021-03-05 20:55:58 -0600

    just trying to figure out IntelliJ

--- src/MainmyHelloWorld.java
+++ src/MainmyHelloWorld.java
@@ -2,7 +2,9 @@ public class MainmyHelloWorld {
     public static void main (String []args){
         System.out.println("Print this Hello World!!!");
 
-        // use git diff --staged to see changes in staged
+        // use git diff --staged to see changes in stage
+
+        // just adding a note to see how add works in intelliJ
     }
 
 }
